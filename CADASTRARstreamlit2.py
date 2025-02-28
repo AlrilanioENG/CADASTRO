d71bbb3aa6c7e848475f6325c0d52377cea9a894
AlrilanioENG <alrilaniojose@gmail.com>
2025-02-27 23:03:56 -0300

    Add files via upload

---
+++ CADASTRARstreamlit2.py
@@ -0,0 +1,18 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+import os
+credentials = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(os.getcwd(), 'lojapratasheets-6cb2a4d5f428.json'), scope)
+
+client = gspread.authorize(credentials)
+
+spreadsheet = client.open('peças cadastradas')
+sheet = spreadsheet.worksheet('Página1')
+
+code = input('Digite o código da peça: ')
+desc = input('Digite a descrição da peça: ')
+dados = [code, desc]
+
+sheet.append_row(dados)
+print('✅ Dados enviados com sucesso para o Google Sheets!')
